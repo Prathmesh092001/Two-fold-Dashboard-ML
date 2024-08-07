0f10aa306991eebcb60bb0fad31c574ee166e00b
Prathmesh092001 <prathmesh.22msd7035@vitapstudent.ac.in>
2024-08-07 19:39:00 +0530

    Updated config file

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost/mydatabase')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost/mydatabase')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'super-secret-key'
